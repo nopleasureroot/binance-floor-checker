c4ded940f10bc3035c3e48d3971a1fbbd358de76
vladimir <snigereff.v@yandex.ru>
2022-01-09 19:05:15 +0300

    change java version

--- src/main/java/com/nomercy/binancefloorchecker/utils/Extractor.java
+++ src/main/java/com/nomercy/binancefloorchecker/utils/Extractor.java
@@ -3,15 +3,15 @@ package com.nomercy.binancefloorchecker.utils;
 import com.nomercy.binancefloorchecker.model.DefaultCommandModel;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
+import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 
 public class Extractor {
     public static DefaultCommandModel extractCommandAndArgs(Message message) {
         DefaultCommandModel defaultCommandModel = new DefaultCommandModel();
 
         defaultCommandModel.setCommand(message.getEntities().get(0).getText());
-        defaultCommandModel.setArgs(new LinkedList<>(List.of(message.getText().split(" ")).subList(1, message.getText().split(" ").length)));
+        defaultCommandModel.setArgs(new LinkedList<>(Arrays.asList(message.getText().split(" ")).subList(1, message.getText().split(" ").length)));
 
         return defaultCommandModel;
     }
